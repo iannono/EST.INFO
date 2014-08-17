52db3d4bf4b577f47e6e6a6eabb53c5f162678cb
xiongbo <xiongbo027@gmail.com>
2014-08-17 16:34:46 +0800

    update others with entry image handle

--- lib/crawler/base.rb
+++ lib/crawler/base.rb
@@ -11,3 +11,13 @@ require './lib/robots/twitter'
 
 dbconfig = YAML::load(File.open('./config/database.yml'))
 ActiveRecord::Base.establish_connection(dbconfig["development"])
+
+def update_entry_img(entry) 
+  if entry.img.nil? && entry.images.size > 0
+    image = entry.images.first
+    entry.img = image.img_link
+    entry.img_name = image.img_name
+    entry.save
+  end
+end
+
