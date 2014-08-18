a020646cd6029b891c35a1da7e4e69718756ca53
caok <caok1231@gmail.com>
2014-08-18 12:58:29 +0800

    fix the bug of delayed_job

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :scm, :git
 # set :pty, true
 
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
