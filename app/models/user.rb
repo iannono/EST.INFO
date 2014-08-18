68204ff3fec4483c331b7dfadf69ecd2b4be173e
caok <caok1231@gmail.com>
2014-08-18 12:13:15 +0800

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates_uniqueness_of :email
-  after_create :send_welcome_email
+  #after_create :send_welcome_email
 
   def send_welcome_email
     UserMailer.delay.welcome(self)
