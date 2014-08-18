209e62eeb15b9203edaae0f1ddee5de09dedb2f6
caok <caok1231@gmail.com>
2014-08-18 18:45:37 +0800

    update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,17 @@
 set :output, 'log/cron.log'
+
 every 5.minutes do
-  rake "crawler:dgtle"
-  rake "crawler:macx"
   rake "crawler:feng"
+end
+
+every 8.minutes do
   rake "crawler:v2ex"
+end
+
+every 10.minutes do
+  rake "crawler:macx"
+end
+
+every 15.minutes do
+  rake "crawler:dgtle"
 end
