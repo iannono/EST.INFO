f43ef8b0458a43d2010ac043b8c32010d53656f0
caok <caok1231@gmail.com>
2014-09-17 11:55:30 +0800

    add search_link for category and stopPropagation

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,7 @@
 class EntriesController < ApplicationController
   def index
     @q = params[:q].present? ? Entry.search(params[:q]) : Entry.search
+    @q.category_eq = params[:category] if params[:category].present?
     @entries = @q.result
     @entries = @entries.page params[:page]
   end
